ab61dd6168705fe2ccf005168ad6b16df508a43a
zhangzongjie <beckhamzzj@163.com>
2025-02-14 20:33:58 +0900

    rm config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-# SBI账户信息
-SBI_USERNAME = '323-0027944'
-SBI_PASSWORD = 'DAXKMNZT'
-
-# Excel文件路径
-EXCEL_FILE_PATH = '/Users/zhangzongjie/Documents/stock/SBI资产记录.xlsx'
-
-# Chrome用户数据目录
-CHROME_USER_DATA_DIR = '/Users/zhangzongjie/workspace/SBI/chrome_profile' 
